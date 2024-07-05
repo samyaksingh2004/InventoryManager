323fcaf848c4794f1316e5ded789ff68d5794434
samyaksingh2004 <samyak.singh@gmail.com>
2024-07-05 20:31:50 +0530

    Initial commit

---
+++ src/main/java/com/example/appengine/InventoryManager/InventoryManagerApplication.java
@@ -0,0 +1,14 @@
+package com.example.appengine.InventoryManager;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class InventoryManagerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(InventoryManagerApplication.class, args);
+	}
+
+	
+}
---
+++ src/test/java/com/example/appengine/InventoryManager/InventoryManagerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.appengine.InventoryManager;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class InventoryManagerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
